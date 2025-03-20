0a88a083c8ec106267acfeb5f9db3ca901cd551a
Danilo Trombino <danilotrombino@gmail.com>
2025-03-20 15:17:16 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
-$db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_name = getenv('DB_NAME') ?: 'image_catalog';
+$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
+$db_pass = getenv('DB_PASS') ?: 'wrong_password';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
